59b4aea7e048d06fbcf9f39299ea8aa537a04bad
DEV <DEV@NSN1191261168.edu119.local>
2026-03-06 16:25:41 -0300

    meu primeiro commit

---
+++ src/routes/usuario.route.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import * as usuarioController from "../controllers/usuario.controller.js"
+
+const router = Router();
+
+
+
+
+router.get("/",usuarioController.listar)
+router.get("/:id",usuarioController.buscarPorId)
+router.post("/",usuarioController.criar)
+router.post("/login", usuarioController.login) //add
+
+export default router;
